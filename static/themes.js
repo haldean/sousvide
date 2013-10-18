8ae666c420c11044cd0c15289a0b6fb93dbc0cad
Will Haldean Brown <will.h.brown@gmail.com>
2013-10-17 17:15:21 -0700

    Change blue secondary so black text looks better against it

--- static/themes.js
+++ static/themes.js
@@ -1,13 +1,13 @@
-var primary, secondary, graph
+var primary, secondary, highlight
 
-function setTheme(p, s, g) {
+function setTheme(p, s, h) {
 	primary = p
 	secondary = s
-	graph = g
+	highlight = h
 	reapplyTheme();
 }
 
-function setBlue() {   setTheme("#5116FE", "#45317E", "#000"); }
+function setBlue() {   setTheme("#5116FE", "#8A98FE", "#000"); }
 function setOrange() { setTheme("#FE4F00", "#FFB213", "#000"); }
 
 function reapplyTheme() {
@@ -16,6 +16,6 @@ function reapplyTheme() {
 	d3.selectAll('.fg-primary').transition().style('color', primary)
 	d3.selectAll('.fg-secondary').transition().style('color', secondary)
 	d3.selectAll('.targetline').transition().style('stroke', secondary)
-	d3.selectAll('.line').transition().style('stroke', graph)
+	d3.selectAll('.line').transition().style('stroke', highlight)
 }
 
